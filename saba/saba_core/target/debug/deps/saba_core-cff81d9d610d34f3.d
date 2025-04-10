96070cb8a0f0541af00ca1647cfd80bf1d324fe0
YukaMurata <mrt.yka.i@gmail.com>
2025-04-10 23:07:54 +0900

    add: section3

---
+++ saba/saba_core/target/debug/deps/saba_core-cff81d9d610d34f3.d
@@ -0,0 +1,8 @@
+/Users/muratayuka/sand_box/rust_sandbox/saba/saba_core/target/debug/deps/saba_core-cff81d9d610d34f3: src/lib.rs src/url.rs src/http.rs src/error.rs
+
+/Users/muratayuka/sand_box/rust_sandbox/saba/saba_core/target/debug/deps/saba_core-cff81d9d610d34f3.d: src/lib.rs src/url.rs src/http.rs src/error.rs
+
+src/lib.rs:
+src/url.rs:
+src/http.rs:
+src/error.rs:
---
+++ saba/saba_core/target/debug/deps/saba_core-e88f0db8f2cb13e9.d
@@ -0,0 +1,10 @@
+/Users/muratayuka/sand_box/rust_sandbox/saba/saba_core/target/debug/deps/libsaba_core-e88f0db8f2cb13e9.rmeta: src/lib.rs src/url.rs src/http.rs src/error.rs
+
+/Users/muratayuka/sand_box/rust_sandbox/saba/saba_core/target/debug/deps/libsaba_core-e88f0db8f2cb13e9.rlib: src/lib.rs src/url.rs src/http.rs src/error.rs
+
+/Users/muratayuka/sand_box/rust_sandbox/saba/saba_core/target/debug/deps/saba_core-e88f0db8f2cb13e9.d: src/lib.rs src/url.rs src/http.rs src/error.rs
+
+src/lib.rs:
+src/url.rs:
+src/http.rs:
+src/error.rs:
